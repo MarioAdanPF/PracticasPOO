527f7f0a5d3a037948d50f70f63e33e3c82854bb
MarioAdanPF <pmarioadan@gmail.com>
2022-02-18 08:31:41 -0600

    Practica04 8:31
    Hora de inicio 7:45

---
+++ Practica04/Personaje.java
@@ -0,0 +1,25 @@
+public class Personaje {
+    private String nombre;
+    private int edad;
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public void saludar(){
+        System.out.println("Hola alumno de POO " + nombre);
+    }
+    public int getEdad(){
+        return edad;
+    }
+    public boolean setEdad(int edad){
+        if ((edad > 0) && (edad < 120)) {
+            this.edad = edad;
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
