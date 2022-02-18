aabd19ff565a3c880f615d9a6549498fad3e321e
MarioAdanPF <pmarioadan@gmail.com>
2022-02-18 09:42:55 -0600

    Paso 4, 5

---
+++ Practica05/src/poo1pvsz/personajes/Personaje.java
@@ -0,0 +1,28 @@
+public class Personaje {
+    private String nombre;
+    private int edad;
+
+    public Personaje(String nombre){
+        this.nombre = nombre;
+    }
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public void saludar(){
+        System.out.println("Hola alumno de POO " + nombre);
+    }
+    public int getEdad(){
+        return edad;
+    }
+    public boolean setEdad(int edad){
+        if ((edad > 0) && (edad < 120)) {
+            this.edad = edad;
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
