5985c35b41e308c965635587019318fc629f147d
MarioAdanPF <pmarioadan@gmail.com>
2022-02-25 08:11:05 -0600

    Practica06 8:10

---
+++ Practica06/edu/issoft/pvsz/personajes/Personaje.java
@@ -0,0 +1,29 @@
+package org.gamex.poo1pvsz.personajes;
+public class Personaje {
+    private String nombre;
+    private int edad;
+
+    public Personaje(String nombre){
+        this.nombre = nombre;
+    }
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public void saludar(){
+        System.out.println("Hola alumno de POO " + nombre);
+    }
+    public int getEdad(){
+        return edad;
+    }
+    public boolean setEdad(int edad){
+        if ((edad > 0) && (edad < 120)) {
+            this.edad = edad;
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
