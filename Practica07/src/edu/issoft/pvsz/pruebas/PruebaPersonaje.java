9af4d99e1582baf2c928ecdce0fca2b2a43d1a9b
MarioAdanPF <pmarioadan@gmail.com>
2022-03-04 10:10:36 -0600

    10:10

--- Practica07/src/edu/issoft/pvsz/pruebas/PruebaPersonaje.java
+++ Practica07/src/edu/issoft/pvsz/pruebas/PruebaPersonaje.java
@@ -11,12 +11,32 @@ public class PruebaPersonaje{
         int n = 0;
         int m = 0;
         
-        while (n <= m){
+        while (n >= m){
             n = ed;
+            ed = (int) (Math.random() * 100);
             m = ed;
                 
             for (int i = n; i >= m; i++){
-                per01.setEdad(ed);
+                ed = (int) (Math.random() * 250);
+                if (per01.setEdad(ed) == True){
+                    System.out.println(per01.getNombre() + " " + per01.getEdad() + " se modificó la edad");
+                }else{
+                    System.out.println(per01.getNombre() + " " + per01.getEdad() + " sin modificaciones en edad a " + ed);
+                }
+
+                ed = (int) (Math.random() * 250);
+                if (per02.setEdad(ed) == True){
+                    System.out.println(per02.getNombre() + " " + per02.getEdad() + " se modificó la edad");
+                }else{
+                    System.out.println(per02.getNombre() + " " + per02.getEdad() + " sin modificaciones en edad a " + ed);
+                }
+
+                ed = (int) (Math.random() * 250);
+                if (per03.setEdad(ed) == True){
+                    System.out.println(per03.getNombre() + " " + per03.getEdad() + " se modificó la edad");
+                }else{
+                    System.out.println(per03.getNombre() + " " + per03.getEdad() + " sin modificaciones en edad a " + ed);
+                }
             }
         }
     }   
