1c1c717521d4bbf2d72145292da2aaa9e402137e
MarioAdanPF <pmarioadan@gmail.com>
2022-03-04 08:41:06 -0600

    8:41

--- Practica07/src/edu/issoft/pvsz/pruebas/PruebaPersonaje.java
+++ Practica07/src/edu/issoft/pvsz/pruebas/PruebaPersonaje.java
@@ -7,5 +7,17 @@ public class PruebaPersonaje{
         Personaje per03 = new Personaje("Rosario");
         Personaje per04 = new Personaje("Antonio");
         
+        int ed = (int) (Math.random() * 100);
+        int n = 0;
+        int m = 0;
+        
+        while (n <= m){
+            n = ed;
+            m = ed;
+                
+            for (int i = n; i >= m; i++){
+                per01.setEdad(ed);
+            }
+        }
     }   
 }
