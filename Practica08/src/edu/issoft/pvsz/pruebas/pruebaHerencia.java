d776128adbc748a8e6a14ba6762eedabbfa8fa6b
MarioAdanPF <pmarioadan@gmail.com>
2022-03-11 11:20:36 -0600

    Practica08 11:20

--- Practica08/src/edu/issoft/pvsz/pruebas/pruebaHerencia.java
+++ Practica08/src/edu/issoft/pvsz/pruebas/pruebaHerencia.java
@@ -1,3 +1,5 @@
+import edu.issoft.pvsz.personajes.Personaje;
+
 public class pruebaHerencia {
     public static void main(String[] args) {
         Personaje per01 = new Personaje("Adrian",100);
@@ -9,5 +11,20 @@ public class pruebaHerencia {
         Zombie zom01 = new Zombie("Armando",80,false);
         Zombie zom02 = new Zombie("Areli",true);
         Zombie zom03 = new Zombie("Gerardo");
+        
+        Personaje personajes [] = new Personaje[8];
+        personajes[0] = per01;
+        personajes[1] = per02;
+        personajes[2] = plan01;
+        personajes[3] = plan02;
+        personajes[4] = plan03;
+        personajes[5] = plan04;
+        personajes[6] = zom01;
+        personajes[7] = zom02;
+        personajes[8] = zom03;
+        for (int i = 0 ; i < personajes.length ; i++){
+            
+        }
+
     }
 }
