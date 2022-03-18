31bc6f151a141064419b76dc55be7b20df15c61b
MarioAdanPF <pmarioadan@gmail.com>
2022-03-18 10:06:11 -0600

    Practica09 10:06

---
+++ Practica09/src/mx/uaz/ingsoft/appsw/universitarios/alumnos/Alumno.java
@@ -0,0 +1,23 @@
+package mx.uaz.ingsoft.appsw.universitarios.alumnos;
+import mx.uaz.ingsoft.appsw.universitarios.Universitario;
+
+public class Alumno extends Universitario{
+    private int calificacion;
+
+    public boolean calificacionEstandar(int calificacion){
+        if ((calificacion >= 0) && (calificacion <= 10)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public Alumno(String nombre, int calificacion){
+        super(nombre);
+        if (calificacionEstandar(calificacion) == true){
+            this.calificacion = calificacion;
+        }else{
+            this.calificacion = 5;
+        }
+    }
+}
