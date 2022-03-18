98a5186cd89aa046e1c8a7c128e4a1db5134b9e0
MarioAdanPF <pmarioadan@gmail.com>
2022-03-18 08:37:22 -0600

    Practica09 8:37

--- Practica09/src/mx/uaz/ingsoft/appsw/universitarios/Universitario.java
+++ Practica09/src/mx/uaz/ingsoft/appsw/universitarios/Universitario.java
@@ -3,6 +3,25 @@ public class Universitario{
     private String nombre;
 
     public Universitario(String nombre){
-        this.nombre=nombre;
+        this.nombre = nombre;
+    }
+
+    public boolean equals(Object o){
+        boolean result = false;
+        if((o != null) && (o instanceof Universitario)){
+            Universitario u = (Universitario) o;
+            if (u.equals(o)){
+                return true;
+            }
+        }
+        return result;
+    }
+
+    public int hashCode(){
+        return nombre.hashCode();
+    }
+    
+    public String toString(){
+        return nombre;
     }
 }
