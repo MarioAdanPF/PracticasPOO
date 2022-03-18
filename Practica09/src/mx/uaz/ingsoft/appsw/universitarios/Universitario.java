9ee469fc68737decd046fde1c7e7ddb4d800f63b
MarioAdanPF <pmarioadan@gmail.com>
2022-03-18 08:03:04 -0600

    Practica09 8:02

---
+++ Practica09/src/mx/uaz/ingsoft/appsw/universitarios/Universitario.java
@@ -0,0 +1,8 @@
+package mx.uaz.ingsoft.appsw.universitarios;
+public class Universitario{
+    private String nombre;
+
+    public Universitario(String nombre){
+        this.nombre=nombre;
+    }
+}
